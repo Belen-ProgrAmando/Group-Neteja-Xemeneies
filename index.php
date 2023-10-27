dd718e19308ec748a7ad30a02068851eaa53d2fc
Belen-progrAmando <belenbarrientos.1985@gmail.com>
2023-10-27 12:33:33 +0200

    estructura y desarrollo

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!--style introduccion h3-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Satisfy&display=swap" rel="stylesheet">
+    
+    <title>Neteja Xemeneies</title>
+</head>
+<body>
+    <header class="sticky-header">
+        <?php
+        include("menu_colores.html");
+        ?>
+    </header>
+
+    <main>
+
+    <div id="contenedor_banner">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicadores de carrusel (Opcionales) -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    
+    <!-- Diapositivas de carrusel -->
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/banner1.png" alt="Slide 1">
+        </div>
+        <div class="carousel-item">
+            <img src="img/banner2.png" alt="Slide 2">
+        </div>
+        <div class="carousel-item">
+            <img src="img/banner3.png" alt="Slide 3">
+        </div>
+    </div>
+    
+    <!-- Controles del carrusel -->
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+        </a>
+    </div>
+    </div>
+
+    <div id="introduccion">   
+        <h3>Servei de Neteja de Conductes de Fum, dissenyat per revolucionar la manera com vius i respires a casa teva.</h3>
+        <h3>… Experiència de més de 18 anys…</h3>  
+        <h1>¿Algunes vegades has considerat la qualitat de l’aire que respires a casa teva?</h1>
+        <h4>Els conductes de fum juguen un paper fonamental per mantenir el teu espai vital saludable 
+        i lliure de contaminants. A Neteja
+         xemeneies Sergi, abans Grup Neteges SB, entenem la importància de mantenir els teus 
+         conductes de fum en condicions òptimes.</h4>
+    </div>
+   
+
+    <div id="container_card">
+        <h1> Neteja i manteniment de tota mena de conductes de fum</h1>
+        <h2>Industriales i Domèstiques</h2>
+
+    <div class="row">
+    <div class="col-md-4">
+      <!-- Tarjeta 3 -->
+      <div class="card" style="background-color:rgb(220, 111, 22); width: 18rem; border: none;">
+        <!-- Contenido de la tarjeta -->
+        <img src="img/3.png" class="img_card">
+        <h6>Xemeneia, estufes, calderes, etc. de totes les marques, models i estils que siguin</h6>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <!-- Tarjeta 4 -->
+      <div class="card" style="background-color:rgb(220, 111, 22); width: 18rem; border: none;">
+        <!-- Contenido de la tarjeta -->
+        <img src="img/4.png" class="img_card">
+        <h6>Sortides fum de pizzeries i braseries, estufes ferro colat, pèl.let, extracció de nius i col·locació de malla protectora</h6>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <!-- Tarjeta 5 -->
+      <div class="card" style="background-color: rgb(220, 111, 22); width: 18rem; border: none;">
+        <!-- Contenido de la tarjeta -->
+        <img src="img/5.png" class="img_card">
+        <h6>Tota mena Forns de tota classe calderes gasoil, carbó, llenya,etc</h6>
+      </div>
+    </div>
+  </div>
+    </div>
+
+    <div id="promocion">
+    <div class="row">
+    <div class="col-12 col-md-6">
+<div class="card" style="background-color: rgb(220, 111, 22); width: 30rem; border: none;">
+      <img src="img/Neteja Xemeneia.jpg">
+</div>      
+</div>
+
+<div class="col-12 col-md-6">
+<div class="card_text" style="background-color: rgb(220, 111, 22); width: 30rem; border: none;">    
+    
+    <ul>
+        <h2>¿Per què escollir Xemeneies Sergi?</h2>
+        <li>
+            <h4>Brindem un Servei Ràpid i convenient; Valorem el teu Temps!</h4>
+        </li>
+        <li>
+            <h4>El nostre equip d'experts garanteix que el vostre sistema de calefacció 
+                funcioni de manera segura, reduint el risc d'incendis i fuites de monòxid de carboni.</h4>
+        </li>
+        <li>
+            <h4> Utilitzem equips de neteja de darrera generació que redueixen
+                 al màxim la generació de residus. Això vol dir que la teva llar o negoci 
+                 es mantindrà net i ordenat durant tot el procés.</h4>
+        </li>
+        <li>
+            <h4>La nostra reputació es basa en la satisfacció dels nostres clients i la seguretat que brindem.</h4>
+        </li>
+    </ul>
+</div>
+</div>
+
+    </div>    
+    </div>
+
+
+  <div id="container_informacion">
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <!-- Contenido de la primera columna -->
+        <h2>"Manteniment de Calderes a la Indústria: Claus per a l'Èxit"</h2>
+    <br>
+    <h4>Com el manteniment de la caldera garanteix un funcionament eficient i la continuïtat de la producció</h4>
+    <br>
+    <p class="text_info">El manteniment de calderes a la indústria és un component crític per assegurar que els processos de producció
+funcionin sense problemes. En aquest article, explorarem les claus per a l'èxit en el manteniment de calderes a
+la indústria, incloent-hi la importància de la programació regular, la formació del personal i les pràctiques de seguretat.
+  Aprendràs com el manteniment adequat de les calderes pot optimitzar
+</p>
+<button class="leer-mas">Leer más...</button>
+
+<div class="contenido-oculto" style="display: none;">
+  <!-- Contenido adicional que estará oculto -->
+  <p>l'eficiència dels processos i garantir la seguretat al lloc de treball.
+Un dels aspectes fonamentals del manteniment de calderes a la indústria és la programació regular. El manteniment programat permet identificar
+i abordar problemes potencials abans que es converteixin en falles costoses. Això inclou la neteja de la caldera, la inspecció de components crítics
+  i la realització de proves de rendiment. La programació regular és essencial per prevenir temps d'inactivitat no planificats i garantir-ne la continuïtat
+  de la producció. La formació del personal també és un factor clau en el manteniment de les calderes. És crucial comptar amb personal capacitat i certificat que comprengui
+els procediments de manteniment i les pràctiques de seguretat. A més, el personal ha d'estar al corrent de les regulacions i normatives aplicables per assegurar
+  el compliment normatiu.
+La seguretat en el lloc de treball és molt important quan es tracta de calderes industrials. El manteniment de calderes sovint implica treballar amb equips
+  perillosos, per la qual cosa és essencial seguir pràctiques de seguretat rigoroses. Això inclou l'ús d'equip de protecció personal, la senyalització adequada i la implementació
+  de procediments de seguretat a la feina. La seguretat és una prioritat per garantir que els treballadors estiguin protegits i que el procés de producció es desenvolupi
+  sense incidents.
+En resum, el manteniment de calderes a la indústria és essencial per garantir l'eficiència operativa i la seguretat al lloc de treball. La programació regular,
+  la formació del
+personal i les pràctiques de seguretat són claus per a lèxit en aquest camp. En invertir en el manteniment adequat de les calderes, les indústries poden optimitzar
+els seus processos de producció i garantir un entorn de treball segur per a tots els empleats.</p>
+</div>
+      </div>
+
+      <script>
+  $(document).ready(function() {
+    $(".leer-mas").click(function(e) {
+      e.preventDefault(); // Evita que el enlace recargue la página
+
+      const contenidoOculto = $(".contenido-oculto");
+      if (contenidoOculto.is(":hidden")) {
+        contenidoOculto.slideDown();
+        $(this).text("Leer menos...");
+      } else {
+        contenidoOculto.slideUp();
+        $(this).text("Leer más...");
+      }
+    });
+  });
+</script>
+
+
+<div class="col-12 col-md-6">
+<div class="card" style="background-color: rgb(220, 111, 22); width: 18rem; border: none;">
+      <img src="img/1.png">
+</div>      
+</div>
+
+<div class="col-12 col-md-6">
+<div class="card" style="background-color: rgb(220, 111, 22); width: 18rem; border: none;">    
+    <img src="img/6.png">
+</div>
+</div>
+     
+
+<div class="col-12 col-md-6">
+        <!-- Contenido de la segunda columna -->
+
+        <h2>La importància de netejar les xemeneies de la llar</h2>
+    <br>
+    <h4>Mantingues la Seguretat i Eficiència a la teva Llar</h4>
+    <br>
+    <p class="text_info">La xemeneia de casa teva és un component essencial per mantenir la teva llar càlida i acollidora durant els mesos més freds de l'any.
+         No obstant això, és fonamental comprendre que, amb el temps, les xemeneies poden acumular sutge, cendres i altres residus que
+         no només disminueixen la seva eficiència, sinó que també poden representar un perill per a la seguretat de la família i la propietat.
+         En aquest article, explorarem per què és crucial netejar regularment les xemeneies, els beneficis que això aporta a casa teva
+    </p>
+    <button class="boton-leer-mas">Leer más...</button>
+
+<div class="contenido-oculto2" style="display: none;">
+  <!-- Contenido adicional que estará oculto -->
+  <p class="text_info">i com ho pots fer de manera segura i efectiva. La neteja regular de xemeneies és essencial per diverses raons. En primer lloc, contribueix significativament a la seguretat del teu
+llar. Al llarg del temps, les xemeneies poden acumular sutge i creosota, que són altament inflamables. Si no s'eliminen regularment,
+  aquests residus poden encendre's i provocar un incendi al conducte de la xemeneia. Un incendi a la xemeneia és un risc greu que
+  no només posa en perill la vida dels qui habiten a la casa, sinó que també pot causar danys estructurals i costosos. Per tant,
+  la neteja de xemeneies és fonamental per prevenir incendis i garantir la seguretat de la teva família. A més de la seguretat, la neteja regular de xemeneies també millora leficiència del teu sistema de calefacció. A mesura que els residus
+s'acumulen al conducte de la xemeneia, disminueix la capacitat de la xemeneia per expulsar el fum i els gasos a l'exterior. Això pot resultar
+en una mala circulació d'aire i en què els fums tòxics entrin a casa teva en lloc de ser expulsats. Com a resultat, el teu sistema de calefacció
+  funcionarà de manera menys eficient, cosa que es traduirà en un major consum de combustible i costos més alts a les teves factures de calefacció. Al
+  netejar regularment la xemeneia, pots mantenir un flux d'aire òptim i aprofitar al màxim el teu sistema de calefacció, cosa que t'estalviarà diners a llarg termini.
+
+La neteja de xemeneies no és una tasca que hagi de ser presa a la lleugera. Si bé alguns propietaris poden intentar fer-ho per ells mateixos,
+és altament recomanable contractar un professional perquè faci la neteja. Els professionals tenen l'experiència i l'equip necessaris
+per dur a terme la neteja de manera segura i efectiva. També poden realitzar inspeccions per detectar possibles danys o problemes a la xemeneia
+que puguin requerir reparació. Contractar un professional és una inversió que val la pena per garantir la seguretat i el rendiment del sistema de calefacció.
+
+En resum, la neteja regular de xemeneies és essencial per garantir la seguretat de casa teva i millorar l'eficiència del teu sistema de calefacció. En prendre mesures
+  per mantenir la xemeneia neta i segura, podeu gaudir d'un ambient càlid i acollidor durant els mesos d'hivern sense preocupar-vos pels riscos d'incendi o
+   els costos innecessaris. Assegura't de programar la neteja de la teva xemeneia de manera regular i considera contractar un professional per fer aquest important servei..</p>
+</div>
+</div>
+
+<script>
+  $(document).ready(function() {
+    $(".boton-leer-mas").click(function(e) {
+      e.preventDefault(); // Evita que el enlace recargue la página
+
+      const contenidoOculto = $(".contenido-oculto2");
+      if (contenidoOculto.is(":hidden")) {
+        contenidoOculto.slideDown();
+        $(this).text("Leer menos...");
+      } else {
+        contenidoOculto.slideUp();
+        $(this).text("Leer más...");
+      }
+    });
+  });
+</script>
+
+</div>
+</div>
+
+<div id="contacto">
+    <div class="style_text_contact">
+    <h3>Ràpids, nets i sense embrutar res</h3>
+    <h1>POSA’T EN CONTACTE</h1>
+    <h2>Programa una visita</h2>
+    </div>
+</div>
+</main>
+  <script src="script.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script> 
+</body>
+<a class="rotate" target="new" href="https://wa.me/message/WVQCF3XHSUCHH1"><img src="img/Whatsapp-logo-vetor.png" alt="whatsapp" width="100%" height="auto"></a>
+
+<footer class="footer">
+    <h6> Copyright © Belen Barrientos ®</h6>
+</footer>
+
+
+</html>
